af721038f291c161ad9aa019be77e4fc5e92ce59
IrfanCatovic <catovicc84@gmail.com>
2026-02-04 12:13:58 +0100

    turned on server

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/books", booksHandler)      //One route and in handler i use POST and GET
+	http.HandleFunc("/books/", booksByIdHandler) //Get specific book by ID
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
